afa7d00dbec616eac56dab7e1e4b11b465729f40
Victoria Fields <victoriaashleyfields@gmail.com>
2022-10-31 10:22:47 -0600

    Add test for user flow adding reservation

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -18,7 +18,7 @@ describe('Turing Cafe Dashboard', () => {
     cy.get('article').contains('7:00')
     cy.get('article').contains('Number of guests: 12')
     cy.get('article > button').should('exist')
-  })
+  });
 
   it('should reflect user inputed data in form inputs', () => {
     cy.intercept('GET', 'http://localhost:3001/api/v1/reservations', {
@@ -31,5 +31,24 @@ describe('Turing Cafe Dashboard', () => {
     cy.get('[name="date"]').type('10/31').should('have.value', '10/31')
     cy.get('[name="time"]').type('7:00').should('have.value', '7:00')
     cy.get('[name="number"]').type('2').should('have.value', '2')
-  })
+  });
+
+  it('user should be able to add a new reservation that displays to the page', () => {
+    cy.intercept('GET', 'http://localhost:3001/api/v1/reservations', {
+      statusCode: 200,
+      ok: true,
+      fixture: "reservations"
+    })
+    cy.visit('http://localhost:3000/')
+    cy.get('[name="name"]').type('Tori')
+    cy.get('[name="date"]').type('10/31')
+    cy.get('[name="time"]').type('7:00')
+    cy.get('[name="number"]').type('2')
+    cy.get('form > button').click()
+    cy.get('article').last().should('exist')
+      .and('contain', 'Tori')
+      .and('contain', '10/31')
+      .and('contain', '7:00')
+      .and('contain', 'Number of guests: 2')
+  });
 })
