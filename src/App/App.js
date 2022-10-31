9b63fb434cd94f1839e9ad84fc981f6ee206f932
Victoria Fields <victoriaashleyfields@gmail.com>
2022-10-31 09:32:15 -0600

    Add GET request and connect fetched data to App state

--- src/App/App.js
+++ src/App/App.js
@@ -3,6 +3,21 @@ import './App.css';
 import ResContainer from '../ResContainer/ResContainer.js'
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      reservations: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3001/api/v1/reservations')
+      .then(response => response.json())
+      .then(data => this.setState({
+        reservations: data
+      }))
+  }
+
   render() {
     return (
       <div className="App">
