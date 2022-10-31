de5dd87081d4194e236344dc1b18054bb38d2731
Victoria Fields <victoriaashleyfields@gmail.com>
2022-10-31 10:12:04 -0600

    Add first Cypress test for dashboard containing DOM elements

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,22 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Turing Cafe Dashboard', () => {
+  it('displays all expected elements on the DOM when user first vists', () => {
+    cy.intercept('GET', 'http://localhost:3001/api/v1/reservations', {
+      statusCode: 200,
+      ok: true,
+      fixture: "reservations"
+    })
+    cy.visit('http://localhost:3000/')
+      .get('.app-title').should('exist').contains('Turing Cafe Reservations')
+    cy.get('[name="name"]').should('exist')
+    cy.get('[name="date"]').should('exist')
+    cy.get('[name="time"]').should('exist')
+    cy.get('[name="number"]').should('exist')
+    cy.get('form > button').should('exist')
+    cy.get('article').should('exist')
+      .get('h2').contains('Christie')
+    cy.get('article').contains('12/29')
+    cy.get('article').contains('7:00')
+    cy.get('article').contains('Number of guests: 12')
+    cy.get('article > button').should('exist')
   })
 })
