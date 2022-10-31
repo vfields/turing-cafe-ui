21b375d797cbebf6bf7f82a153aa6b33da6d5816
Victoria Fields <victoriaashleyfields@gmail.com>
2022-10-31 09:49:01 -0600

    Connect Form state to UI

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -5,13 +5,53 @@ class Form extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      state: 'placeholder'
+      name: '',
+      date: '',
+      time: '',
+      number: ''
     }
+  }
+
+  handleChange = (event) => {
+    const {name, value} = event.target;
+    this.setState({
+      [name]: value
+    })
   }
 
   render() {
     return (
-      <p>I am Form</p>
+      <form>
+        <input
+          type="text"
+          name="name"
+          value={this.state.name}
+          placeholder="Name"
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
+          name="date"
+          value={this.state.date}
+          placeholder="Date (mm/dd)"
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
+          name="time"
+          value={this.state.time}
+          placeholder="Time"
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
+          name="number"
+          value={this.state.number}
+          placeholder="Number of guests"
+          onChange={this.handleChange}
+        />
+        <button>Make Reservation</button>
+      </form>
     )
   }
 }
