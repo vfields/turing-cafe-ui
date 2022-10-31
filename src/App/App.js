ac2e01a503fdeec6d999a7e632b5a68d3ef05e5d
Victoria Fields <victoriaashleyfields@gmail.com>
2022-10-31 10:44:14 -0600

    Refactor POST request for adding reservations

--- src/App/App.js
+++ src/App/App.js
@@ -20,9 +20,19 @@ class App extends Component {
   }
 
   addReservation = (reservation) => {
-    this.setState({
-      reservations: [...this.state.reservations, reservation]
+    fetch('http://localhost:3001/api/v1/reservations', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(reservation)
     })
+      .then(response => response.json())
+      .then(newReservation => {
+        this.setState({
+          reservations: [...this.state.reservations, reservation]
+        })
+      })
   }
 
   render() {
